0a742d5ad6cf6a59db33e409ab23b1703a18552f
Haykaybe3 <haykaybee3@gmail.com>
2026-01-03 22:12:31 +0100

    Clean up repository for private use - remove unused contracts and sensitive docs

--- src/interfaces/IDesignerRegistry.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-interface IDesignerRegistry {
-    function isVerifiedDesigner(address designer) external view returns (bool);
-    function getDesignerWallet(address designer) external view returns (address);
-}
--- src/interfaces/IFileManager.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-interface IFileManager {
-    function getIpfsCid(bytes32 fileHash) external view returns (string memory);
-    function exists(bytes32 fileHash) external view returns (bool);
-}
